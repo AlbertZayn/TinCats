b5af1b32feece960805114c5b95eebfee268613a
Albert Zayn <108021063+AlbertZayn@users.noreply.github.com>
2022-12-22 17:56:19 +0700

    Add .js file via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+document.querySelector(".enlang").addEventListener("click", function refreshPage(){
+    location.reload();
+} 
+);
+
+document.querySelector(".rulang").addEventListener("click", function ruswitch() {
+    document.querySelector(".contacts").innerHTML = "Контакты";
+    document.querySelector(".pricing").innerHTML = "Цены";
+    document.querySelector(".download").innerHTML = "Загрузить";
+    document.querySelector(".big-heading").innerHTML = "Найди  новую и интересную пару для своего питомца";
+    document.querySelector(".big-heading").style.fontSize = "2.70rem";
+    document.querySelector(".big-heading").style.lineHeight = "1.665";
+    document.querySelector(".apple").innerHTML = '<i class = "bi bi-apple"></i> Загрузить';
+    document.querySelector(".android").innerHTML = '<i class = "bi bi-google-play"></i> Загрузить';
+    document.querySelector(".h3-features-1").innerHTML = "Легко использовать";
+    document.querySelector(".h3-features-2").innerHTML = "Элитная клиентура";
+    document.querySelector(".h3-features-3").innerHTML = "Гарантия работы";
+    document.querySelector(".par-features-1").innerHTML = "Легко настолько, что справится даже твой усатый питомец";
+    document.querySelector(".par-features-2").innerHTML = "У нас есть все коты и кошки, лучшие усатики";
+    document.querySelector(".par-features-3").innerHTML = "Найди любовь для своего питомца, либо получи деньги назад";
+    document.querySelector(".testimonials-heading-1").innerHTML = "Больше не нужно надеяться на то, что твой человек найдёт тебе некрасивую пару по объявлению о случке...";
+    document.querySelector(".testimonials-heading-2").innerHTML = "Моя кошка раньше была одинокой, но с помощью TinCats она нашла любовь всех своих 9ти жизней";
+    document.querySelector(".pricing-heading-1").innerHTML = "План для нужд каждого кошачьего";
+    document.querySelector(".pricing-heading-p-1").innerHTML = "Простой и доступный план для тебя и твоего питомца";
+    document.querySelector(".mongrel").innerHTML = "Дворняга";
+    document.querySelector(".breed").innerHTML = "Породистый";
+    document.querySelector(".thoroughbred").innerHTML = "Чистокровный";
+    document.querySelector(".pricing-plan-1").innerHTML = "Бесплатно";
+    document.querySelector(".pricing-plan-2").innerHTML = "$2 / мес";
+    document.querySelector(".pricing-plan-3").innerHTML = "$5 / мес";
+    document.querySelector(".mongrel-p-1").innerHTML = "50 найденных пар в день";
+    document.querySelector(".mongrel-p-2").innerHTML = "100 сообщений в день";
+    document.querySelector(".mongrel-p-3").innerHTML = "Безлимитное использование";
+    document.querySelector(".breed-p-1").innerHTML = "Безлимитное количество пар";
+    document.querySelector(".breed-p-2").innerHTML = "Безлимитные сообщения";
+    document.querySelector(".breed-p-3").innerHTML = "Безлимитное использование";
+    document.querySelector(".thoroughbred-p-1").innerHTML = "Приоритетный список";
+    document.querySelector(".thoroughbred-p-2").innerHTML = "Безлимитное количество пар";
+    document.querySelector(".thoroughbred-p-3").innerHTML = "Безлимитные сообщения";
+    document.querySelector(".thoroughbred-p-4").innerHTML = "Безлимитное использование";
+    document.querySelector(".big-heading-bottom").innerHTML = "Найдите настоящую любовь для своей питомца прямо сейчас";
+    document.querySelector(".em-1").innerHTML = "Пуся";
+    document.querySelector(".em-2").innerHTML = "Альберт";
+    document.querySelector(".apple-btn-bottom").innerHTML = '<i class = "bi bi-apple"></i> Загрузить';
+    document.querySelector(".android-btn-bottom").innerHTML = '<i class = "bi bi-google-play"></i> Загрузить';
+    document.querySelector(".pricing-btn-1").innerHTML = "Регистрация";
+    document.querySelector(".pricing-btn-2").innerHTML = "Регистрация";
+    document.querySelector(".pricing-btn-3").innerHTML = "Регистрация";
+
+} 
+);
+
+
